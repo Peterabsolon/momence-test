33fbc2a51709b9b077dd74425949fcefd6de7594
Peter Absolon <peterabsolon@yahoo.com>
2023-04-04 17:56:58 +0200

    feat: use NPM workspaces

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,12 +7,14 @@ export default defineConfig({
     port: 3000,
   },
 
+  clearScreen: false,
+
   plugins: [
     react({
       babel: {
         plugins: [
           [
-            "babel-plugin-styled-components",
+            'babel-plugin-styled-components',
             {
               ssr: false,
               pure: true,

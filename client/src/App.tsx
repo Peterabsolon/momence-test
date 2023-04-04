33f28995c0e34317a5b170b869bff9d4ebd26a94
Peter Absolon <peterabsolon@yahoo.com>
2023-04-04 12:32:16 +0200

    chore: setup styled-components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import styled from 'styled-components'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,7 +9,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <Wrapper className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,8 +30,12 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </div>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  background: red;
+`
 
 export default App

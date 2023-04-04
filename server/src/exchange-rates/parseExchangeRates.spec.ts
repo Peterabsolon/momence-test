1bb4720b1664a0b8bdc67ad7468b0f02a2105bfc
Peter Absolon <peterabsolon@yahoo.com>
2023-04-04 14:30:26 +0200

    feat: [server] implement parseExchangeRates + test with 100% coverage

--- server/src/exchange-rates/parseExchangeRates.spec.ts
+++ server/src/exchange-rates/parseExchangeRates.spec.ts
@@ -1,11 +1,52 @@
 import { parseExchangeRates } from './parseExchangeRates';
 
 describe('parseExchangeRates', () => {
-  it('returns empty array when data empty', () => {
-    expect(parseExchangeRates('')).toEqual([]);
+  it('returns null when string empty', () => {
+    expect(parseExchangeRates('')).toBe(null);
   });
 
-  it('returns empty array when data malformed', () => {
-    expect(parseExchangeRates('wowsuchmalformeddata')).toEqual([]);
+  it('returns null when string malformed', () => {
+    expect(parseExchangeRates('wowsuchmalformeddata')).toBe(null);
+  });
+
+  it('returns null when date row malformed', () => {
+    expect(parseExchangeRates('wowsuchmalformeddata')).toBe(null);
+  });
+
+  it('returns null when columns row malformed', () => {
+    const rows = ['03 Apr 2023 #66', 'FOO|BAR|BAZ|QUX|FRED'];
+    expect(parseExchangeRates(rows.join('\n'))).toBe(null);
+  });
+
+  it('returns empty array when data rows empty', () => {
+    const rows = ['03 Apr 2023 #66', 'Country|Currency|Amount|Code|Rate'];
+    expect(parseExchangeRates(rows.join('\n'))).toEqual([]);
+  });
+
+  it('returns empty array when data row malformed', () => {
+    const rows = [
+      '03 Apr 2023 #66',
+      'Country|Currency|Amount|Code|Rate',
+      'foobarbaz',
+    ];
+    expect(parseExchangeRates(rows.join('\n'))).toEqual([]);
+  });
+
+  it('returns parsed data when string valid', () => {
+    const rows = [
+      '03 Apr 2023 #66',
+      'Country|Currency|Amount|Code|Rate',
+      'Australia|dollar|1|AUD|14.578',
+    ];
+
+    expect(parseExchangeRates(rows.join('\n'))).toEqual([
+      {
+        country: 'Australia',
+        currency: 'dollar',
+        amount: 1,
+        code: 'AUD',
+        rate: 14.578,
+      },
+    ]);
   });
 });
--- server/src/exchange-rates/parseExchangeRates.ts
+++ server/src/exchange-rates/parseExchangeRates.ts
@@ -1,6 +1,42 @@
 import { ExchangeRate } from './exchange-rates.types';
 
-export const parseExchangeRates = (str: string) => {
-  console.log({ str });
-  return [] as ExchangeRate[];
+export const parseExchangeRates = (str: string): ExchangeRate[] => {
+  if (!str.length || !str.includes('\n')) {
+    return null;
+  }
+
+  const rows = str.split('\n');
+
+  const columnsRow = rows[1];
+  if (columnsRow !== 'Country|Currency|Amount|Code|Rate') {
+    return null;
+  }
+
+  const dataRows = rows.slice(2, rows.length);
+  if (!dataRows.length) {
+    return [];
+  }
+
+  return dataRows.map(parseExchangeRate).filter(Boolean);
+};
+
+const parseExchangeRate = (row: string): ExchangeRate | null => {
+  const parts = row.split('|');
+  if (parts.length !== 5) {
+    return null;
+  }
+
+  const country = parts[0];
+  const currency = parts[1];
+  const amount = Number(parts[2]);
+  const code = parts[3];
+  const rate = Number(parts[4]);
+
+  return {
+    country,
+    currency,
+    amount,
+    code,
+    rate,
+  } as ExchangeRate;
 };

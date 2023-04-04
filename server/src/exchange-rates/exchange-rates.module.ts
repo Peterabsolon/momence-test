8ced81512d419250c27176b128fcb70ea6fcc6bd
Peter Absolon <peterabsolon@yahoo.com>
2023-04-04 13:19:16 +0200

    feat: [server] add fetch exchange data endpoint

--- server/src/exchange-rates/exchange-rates.module.ts
+++ server/src/exchange-rates/exchange-rates.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+
 import { ExchangeRatesController } from './exchange-rates.controller';
 import { ExchangeRatesService } from './exchange-rates.service';
 
 @Module({
+  imports: [HttpModule],
   controllers: [ExchangeRatesController],
   providers: [ExchangeRatesService],
 })
--- server/src/exchange-rates/exchange-rates.service.ts
+++ server/src/exchange-rates/exchange-rates.service.ts
@@ -1,4 +1,18 @@
+import { Observable, map } from 'rxjs';
+import { AxiosResponse } from 'axios';
+
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 
+// TODO: Move somewhero elso
+const API_URL =
+  'https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt';
+
 @Injectable()
-export class ExchangeRatesService {}
+export class ExchangeRatesService {
+  constructor(private readonly httpService: HttpService) {}
+
+  findAll(): Observable<AxiosResponse<any[]>> {
+    return this.httpService.get(API_URL).pipe(map((res) => res.data));
+  }
+}

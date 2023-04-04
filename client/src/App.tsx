d010e5f6a3df305c21986d82ac6a5d4115fd85a6
Peter Absolon <peterabsolon@yahoo.com>
2023-04-04 22:13:10 +0200

    debug: log .env

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,12 @@ import { QueryClientProvider } from 'react-query'
 import { queryClient } from './lib'
 import { HomePage } from './pages'
 import './App.css'
+import { config } from './config'
+
+const apiUrlFromProcess = process.env.API_URL
+console.log('apiUrlFromProcess', apiUrlFromProcess)
+
+console.log('from config', config.API_URL)
 
 function App() {
   return (

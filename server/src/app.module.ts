4be8eed0647f225f9e4cb49238a9fc2c078b555a
Peter Absolon <peterabsolon@yahoo.com>
2023-04-04 18:49:26 +0200

    fix: [server] reading .env

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ExchangeRatesModule } from './exchange-rates/exchange-rates.module';
 
 @Module({
-  imports: [ExchangeRatesModule],
+  imports: [ExchangeRatesModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
